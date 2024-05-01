3135509bb1bda8aee0dd77034ab3ce8b9e57dee9
Emily Soth <esoth@stanford.edu>
2024-05-01 11:17:21 -0700

    shapely compare macos-13 and 14

--- test.py
+++ test.py
@@ -1,50 +1,6 @@
-import shapely.geometry
-from osgeo import gdal
-from osgeo import ogr
-from osgeo import osr
-import numpy
-import numpy.testing
+import shapely
 
-origin = (444720, 3751320)
-pixel_size = (30, -30)
-srs = osr.SpatialReference()
-srs.ImportFromEPSG(3116)
-
-# Create a test vector
-source_vector_path = 'vector.geojson'
-vector_driver = ogr.GetDriverByName('GeoJSON')
-vector = vector_driver.CreateDataSource(source_vector_path)
-layer = vector.CreateLayer('test', srs=srs, geom_type=ogr.wkbPolygon)
-layer_defn = layer.GetLayerDefn()
-feature = ogr.Feature(layer_defn)
-geometry = ogr.CreateGeometryFromWkb(
-    # invalid geometry: bowtie shape with self-intersection at the center
-    shapely.geometry.Polygon([
-        (
-            origin[0] + pixel_size[0] * x_offset,
-            origin[1] + pixel_size[1] * y_offset
-        ) for x_offset, y_offset in [(0, 0), (0, 1), (0.5, 0.5), (0, 0)]]
-    ).wkb)
-feature.SetGeometry(geometry)
-layer.CreateFeature(feature)
-layer = None
-vector = None
-
-# Create a test raster
-source_raster_path = 'source.tif'
-driver = gdal.GetDriverByName('GTIFF')
-raster = driver.Create(source_raster_path, 1, 1, 1, gdal.GDT_Byte)
-raster.SetProjection(srs.ExportToWkt())
-raster.SetGeoTransform([
-    origin[0], pixel_size[0], 0,
-    origin[1], 0, pixel_size[1]])
-raster = None
-
-# Rasterize
-raster = gdal.OpenEx(source_raster_path, gdal.GA_Update | gdal.OF_RASTER)
-vector = gdal.OpenEx(source_vector_path, gdal.OF_VECTOR)
-layer = vector.GetLayer(0)
-gdal.RasterizeLayer(raster, [1], layer, burn_values=[1], options=['ALL_TOUCHED=FALSE'])
-
-band = raster.GetRasterBand(1)
-numpy.testing.assert_array_equal(band.ReadAsArray(), numpy.array([[1]]))
+a = shapely.from_wkt('POLYGON ((110 110, 110 100, 100 100, 100 110, 110 110))')
+b = shapely.from_wkt('LINESTRING (100 -100, 100 100)')
+shapely.prepare(a)
+print(a.intersection(b))
